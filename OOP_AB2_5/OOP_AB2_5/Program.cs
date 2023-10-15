d77959967485e923b463690c4d72a9e8303a2425
sudosyko <miguel.montag@outlook.com>
2023-10-15 18:54:47 +0200

    test commit

---
+++ OOP_AB2_5/OOP_AB2_5/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Math;
+namespace OOP_AB2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Eingabe;
+            Console.WriteLine("Bitte Zahl Eingeben!");
+            Eingabe = double.Parse(Console.ReadLine());
+            double Ausgabe = Sqrt(Eingabe);
+            Console.WriteLine(Ausgabe);
+        }
+    }
+}
---
+++ OOP_AB2_6/OOP_AB2_6/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.String;
+namespace OOP_AB2_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Eingabe;
+            Console.WriteLine("Bitte E-Mail Eingeben: ");
+            Eingabe = Console.ReadLine();
+
+        }
+    }
+}
