6ca2630bbe7f0cf86dbfe9d17388b3f306ac2e8c
sudosyko <miguel.montag@outlook.com>
2023-10-17 18:41:26 +0200

    Added Aufgabe 2.7 working & tested

---
+++ OOP_AB2_7/OOP_AB2_7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP_AB2_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklaration Liste
+            List<Person> friends = new List<Person>();
+
+            //friends hinzufügen
+            friends.Add(new Person("Tomislav", "Tofilofski", Sprache.Englisch));
+            friends.Add(new Person("Miguel", "Montag", Sprache.Französisch));
+            friends.Add(new Person("Enes", "Besic", Sprache.Französisch));
+            friends.Add(new Person("Leo", "Pieren", Sprache.Deutsch));
+            friends.Add(new Person("Slim", "Shady", Sprache.Englisch));
+
+            //Ausgabe Hallo sagen
+            foreach (Person person in friends)
+            {
+                person.SageHallo();
+            }
+
+        }
+    }
+}
