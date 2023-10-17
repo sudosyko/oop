81a37a3c278e006e3f378519df148bfe27ad0296
sudosyko <miguel.montag@outlook.com>
2023-10-17 18:14:22 +0200

    working & tested Aufgabe 2.6

--- OOP_AB2_6/OOP_AB2_6/Program.cs
+++ OOP_AB2_6/OOP_AB2_6/Program.cs
@@ -5,9 +5,43 @@ namespace OOP_AB2_6
     {
         static void Main(string[] args)
         {
+            // Deklaration Variablen & Defaults
             string Eingabe;
-            Console.WriteLine("Bitte E-Mail Eingeben: ");
+
+            // Aufforderung zur Eingabe
+            Console.WriteLine("Bitte E-Mail Addresse Eingeben: ");
             Eingabe = Console.ReadLine();
+
+            // Prüfung @ Zeichen
+            if (Eingabe.Contains('@'))
+            {
+                // Prüfung ob Empfänger vorhanden
+                if (Eingabe.StartsWith('@'))
+                {
+                    Console.WriteLine("Es fehlt der Empfänger!");
+                }
+                // Prüfung ob Domain vorhanden
+                else if (Eingabe.EndsWith('@'))
+                {
+                    Console.WriteLine("Es fehlt die Domäne!");
+                }
+                // Prüfung ob Domain korrekt
+                else
+                {
+                    if (Eingabe.Contains('.') && Eingabe.IndexOf('@') < Eingabe.IndexOf('.'))
+                    {
+                        Console.WriteLine("Eingabe korrekt");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Domäne nicht korrekt!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Es gibt ein @ Zeichen!");
+            }
 
         }
     }
