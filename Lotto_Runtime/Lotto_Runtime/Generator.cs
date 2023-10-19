567678e747b7d462bc435b9dca836532c1924ee8
sudosyko <miguel.montag@outlook.com>
2023-10-19 16:40:18 +0200

    uebungstest funktionierend

---
+++ Lotto_Runtime/Lotto_Runtime/Generator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lotto_Generator
+{
+    public static class Lotto
+    {
+        /// <summary>
+        /// Runs a Draw of 6 numbers out of 1-42.
+        /// </summary>
+        /// <returns>Bytearray with the 6 drawn numbers</returns>
+        public static byte[] RunLottery()
+        {
+            return DrawNumbers();
+        }
+
+        /// <summary>
+        /// Runs a Poll of 6 random numbers out of 1-42.
+        /// </summary>
+        /// <param name="tips">Bytearray with the 6 drawn numbers (1-42)</param>
+        /// <returns>Amount of hits</returns>
+        public static int RunLottery(byte[] tips)
+        {
+            byte[] drawnNumbers = DrawNumbers();
+            int hits = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (tips[i] == drawnNumbers[j])
+                    {
+                        hits++;
+                        Console.WriteLine("Hit! : " + tips[i]);
+                        break;
+                    }
+                }
+            }
+            return hits;
+        }
+
+        private static byte[] DrawNumbers()
+        {
+            byte[] drawnNumbers = new byte[6];
+            Random random = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                byte hits = (byte)random.Next(1, 46);
+                drawnNumbers[i] = hits;
+            }
+            return drawnNumbers;
+        }
+
+    }
+}
---
+++ Uebungstest/Uebungstest/ConsolePlus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebungstest
+{
+    internal class ConsolePlus
+    {
+
+        public static int WriteRead(string inputFrageAlter)
+        {
+
+            Console.WriteLine(inputFrageAlter);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void WriteUnderline(string output)
+        {
+            Console.WriteLine(output);
+            Console.WriteLine(new string('-', output.Length));
+        }
+
+        
+    }
+}
