69abef2e300ef10fc07a648dc1618628fb51a19a
sudosyko <miguel.montag@outlook.com>
2023-10-19 15:30:29 +0200

    Doesnt work importing classes from projects is shitty documented

---
+++ Lotto_Runtime/Lotto_Runtime/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lotto_Runtime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] draw = Lotto_Generator.Lotto.RunLottery();
+            foreach (byte number in draw)
+            {
+                System.Console.WriteLine("Drawn: "+ number);
+            }
+
+            byte[] myTips = { 5, 9, 12, 31, 19, 20 };
+            int hitAmount = Lotto_Generator.Lotto.RunLottery(myTips);
+            if (hitAmount == 0) System.Console.WriteLine("No Hits you lost!");
+        }
+    }
+}
