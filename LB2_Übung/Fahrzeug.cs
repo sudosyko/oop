c216930fd379783d2e886a62fb3e749e24e5b79c
Miguel Montag <147999768+sudosyko@users.noreply.github.com>
2024-01-18 16:18:22 +0100

    added LB2 Vorbereitung

---
+++ LB2_Übung/Fahrzeug.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waschanlage
+{
+	internal class Fahrzeug
+	{
+		public string Bezeichnung { get; set; }
+		public int Verschmutzungsgrad { get; set; }
+		public void Anzeige()
+		{
+			Console.WriteLine("Fahrzeug: " +  Bezeichnung);
+			if(Verschmutzungsgrad >= 0)
+			{
+				Console.WriteLine("Verschmutzngsgrad : " + Verschmutzungsgrad);
+			}
+			else
+			{
+				Verschmutzungsgrad= 0;
+				Console.WriteLine("Verschmutzngrad : " + Verschmutzungsgrad );
+			}
+			
+		}
+	}
+}
